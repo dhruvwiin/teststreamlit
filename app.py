beeca1f4f7ac5ebd66ebabb0457862ffcbc9d471
dhruvin6215 <62843723+dhruvin6215@users.noreply.github.com>
2025-04-03 12:04:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ import matplotlib.pyplot as plt
 st.title("Hello World App")
 st.write("This is a simple Streamlit app.")
 st.button("Click me!")
-if st.button("Click me!"):
+if st.file_uploader("Upload CSV", type=["csv"]):
     data = pd.read_csv("data.csv")
     st.write(data)
