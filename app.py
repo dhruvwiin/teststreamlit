fa268bc7a5333ac7b7d1a22f31d3849fa5ea5174
dhruvin6215 <62843723+dhruvin6215@users.noreply.github.com>
2025-04-03 12:02:13 -0500

    added requirements and readme

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 
 st.title("Hello World App")
 st.write("This is a simple Streamlit app.")
+st.button("Click me!")
+if st.button("Click me!"):
+    data = pd.read_csv("data.csv")
+    st.write(data)
